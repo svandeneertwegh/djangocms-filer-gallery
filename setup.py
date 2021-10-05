d853b7d54a7629a918fb3f52454059af8b102e19
Stefan van den Eertwegh <stefan@corebyte.nl>
2021-10-05 12:35:11 +0200

    update v2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='corebytecms_gallery',  
-     version='0.2',
+     version='0.3',
      author="Stefan van den Eertwegh",
      author_email="stefan.eertwegh@gmail.com",
      description="Gallery plugin for djangocms",
@@ -14,6 +14,7 @@ setuptools.setup(
      long_description_content_type="text/markdown",
      url="https://github.com/svandeneertwegh/corebytecms-gallery",
      packages=["corebytecms_gallery"],
+     include_package_data = True,
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
