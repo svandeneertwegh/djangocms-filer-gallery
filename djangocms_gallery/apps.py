a18aa931de17a5c598fc7dd8da5721c85b3f36a4
Stefan van den Eertwegh <stefan@corebyte.nl>
2021-10-05 13:16:54 +0200

    update v2

--- djangocms_gallery/apps.py
+++ djangocms_gallery/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class CorebytecmsGalleryConfig(AppConfig):
-    name = 'corebytecms_gallery'
+class GalleryConfig(AppConfig):
+    name = 'djangocms_gallery'
     verbose_name = _('Gallery')
--- djangocms_gallery/cms_plugins.py
+++ djangocms_gallery/cms_plugins.py
@@ -1,14 +1,5 @@
 # coding: utf-8
 
-"""
-    djangocms-widgets
-    ~~~~~~~~~~~~~~~~~
-
-    :copyleft: 2015 by the djangocms-widgets team, see AUTHORS for more details.
-    :created: by JensDiemer.de
-    :license: GNU GPL v3 or above, see LICENSE for more details.
-"""
-
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
@@ -23,7 +14,7 @@ class GalleryPlugin(CMSPluginBase):
     model = ImageGallery
     name = _("Image gallery")
     # text_enabled = True
-    render_template = 'corebytecms_gallery/gallery.html'
+    render_template = 'djangocms_gallery/gallery.html'
 
     def render(self, context, instance, placeholder):
         context = super().render(context, instance, placeholder)
